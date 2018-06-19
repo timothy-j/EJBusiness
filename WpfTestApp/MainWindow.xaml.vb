7a9e0d73b18747dcb29e57cf477d1856b58a61c5
Timothy Jackson <timothy@edwardjacksonltd.com>
2018-06-19 22:10:26 +0100

    Table now fully binds machine columns and opens the order on mc status double click

--- WpfTestApp/MainWindow.xaml.vb
+++ WpfTestApp/MainWindow.xaml.vb
@@ -1,20 +1,36 @@
 ﻿Imports System.Data.Entity
 Imports System.Globalization
+Imports System.Windows.Controls
 
 Class QtyConverter
-    Implements IValueConverter
+    Implements IMultiValueConverter
 
-    Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.Convert
-        Return (From cod In CType(value, EJData.ObservableListSource(Of EJData.CustOrderItemDetail))
+    'Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.Convert
+    '    Return (From cod In CType(value, EJData.ObservableListSource(Of EJData.CustOrderItemDetail))
+    '            Where cod.DetailID = parameter
+    '            Select Qty = cod.Quantity).FirstOrDefault
+    'End Function
+
+    Public Function Convert(values() As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IMultiValueConverter.Convert
+        Return (From cod In CType(values(0), EJData.ObservableListSource(Of EJData.CustOrderItemDetail))
                 Where cod.DetailID = parameter
                 Select Qty = cod.Quantity).FirstOrDefault
+        Throw New NotImplementedException()
     End Function
 
-    Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
-        Dim coid As EJData.CustOrderItemDetail = (From cod In CType(value, EJData.ObservableListSource(Of EJData.CustOrderItemDetail))
+    'Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+    '    Dim coid As EJData.CustOrderItemDetail = (From cod In CType(value, EJData.ObservableListSource(Of EJData.CustOrderItemDetail))
+    '                                              Where cod.DetailID = parameter).FirstOrDefault
+    '    coid.Quantity = value
+    '    Throw New NotImplementedException()
+    'End Function
+
+    Public Function ConvertBack(value As Object, targetTypes() As Type, parameter As Object, culture As CultureInfo) As Object() Implements IMultiValueConverter.ConvertBack
+        Dim coid As EJData.CustOrderItemDetail = (From cod In CType(value(0), EJData.ObservableListSource(Of EJData.CustOrderItemDetail))
                                                   Where cod.DetailID = parameter).FirstOrDefault
         coid.Quantity = value
-        Throw New NotImplementedException()
+        Return Nothing
+        'Throw New NotImplementedException()
     End Function
 End Class
 
@@ -32,29 +48,45 @@ Class StatusConverter
     End Function
 End Class
 
+Class MachineInfo
+    Public Property Number As Short
+    Public Property DetailID As Integer?
+End Class
+
 Class MachInfo
     Inherits DependencyObject
 
-    Public Shared ReadOnly NumberProperty As DependencyProperty = DependencyProperty.RegisterAttached(
-            "Number", GetType(Short), GetType(MachInfo))
+    'Public Shared ReadOnly NumberProperty As DependencyProperty = DependencyProperty.RegisterAttached(
+    '        "Number", GetType(Short), GetType(MachInfo))
 
-    Public Shared ReadOnly DetailIDProperty As DependencyProperty = DependencyProperty.RegisterAttached(
-            "DetailID", GetType(Integer?), GetType(MachInfo))
+    'Public Shared ReadOnly DetailIDProperty As DependencyProperty = DependencyProperty.RegisterAttached(
+    '        "DetailID", GetType(Integer?), GetType(MachInfo))
 
-    Public Shared Function GetNumber(ByVal element As DependencyObject) As Short
-        Return CType(element.GetValue(NumberProperty), Short)
-    End Function
+    Public Shared ReadOnly EntitiesProperty As DependencyProperty = DependencyProperty.RegisterAttached(
+            "Entities", GetType(List(Of Object)), GetType(MachInfo))
 
-    Public Shared Sub SetNumber(ByVal element As DependencyObject, ByVal value As Short)
-        element.SetValue(NumberProperty, value)
-    End Sub
+    'Public Shared Function GetNumber(ByVal element As DependencyObject) As Short
+    '    Return CType(element.GetValue(NumberProperty), Short)
+    'End Function
 
-    Public Shared Function GetDetailID(ByVal element As DependencyObject) As Integer?
-        Return CType(element.GetValue(DetailIDProperty), Integer?)
-    End Function
+    'Public Shared Sub SetNumber(ByVal element As DependencyObject, ByVal value As Short)
+    '    element.SetValue(NumberProperty, value)
+    'End Sub
+
+    'Public Shared Function GetDetailID(ByVal element As DependencyObject) As Integer?
+    '    Return CType(element.GetValue(DetailIDProperty), Integer?)
+    'End Function
+
+    'Public Shared Function GetEntity(ByVal element As DependencyObject) As List(Of Object)
+    '    Return CType(element.GetValue(DetailIDProperty), List(Of Object))
+    'End Function
 
     Public Shared Sub SetDetailID(ByVal element As DependencyObject, ByVal value As Integer?)
-        element.SetValue(DetailIDProperty, value)
+        element.SetValue(EntitiesProperty, value)
+    End Sub
+
+    Public Shared Sub SetEntity(ByVal element As DependencyObject, ByVal value As List(Of Object))
+        element.SetValue(EntitiesProperty, value)
     End Sub
 End Class
 
@@ -62,9 +94,10 @@ Class MainWindow
     Private _db As EJData.CorporateEntities
     Dim ItemViewSource As System.Windows.Data.CollectionViewSource
     Dim MachineType As String
-    'Dim m_machines As List(Of MachInfo)
+    Dim m_machines As New List(Of MachineInfo)
     Private _filterList As New Stack(Of EJFilter)
     Private _filterOn As Boolean
+    Private Delete_Me As Integer? = 0
 
     Structure EJFilter
         Public columnName As String
@@ -107,33 +140,36 @@ Class MainWindow
                        Where m.Type = MachineType
         End If
 
-        Dim MachineList = (From m In machines
-                           Order By m.Number Descending
-                           Select m.Number, m.OrderDetailID).ToList
+        m_machines = (From m In machines
+                      Order By m.Number Descending
+                      Select New MachineInfo With {.Number = m.Number, .DetailID = m.OrderDetailID}).ToList
 
-        For Each mc In MachineList
+        ' Create machine columns for this machine type
+        Dim colNo As Integer = 0
+        For Each mc In m_machines
 
             Dim colS As DataGridTextColumn = ItemDataGrid.FindResource("MCStatusColumn")
             colS.Header = "S" & mc.Number
-            Dim bind = New Binding("CustOrderItemDetails")
-            bind.Converter = New StatusConverter
-            bind.ConverterParameter = mc.OrderDetailID
-            'bind.Mode = BindingMode.OneWay
+            Dim bind = New Binding
+            ' Binds to the Entities attached property, as indexed by colNo
+            bind.Path = New PropertyPath("(0)[(1)].Status", MachInfo.EntitiesProperty, colNo)
+
+            Dim rs As New RelativeSource(RelativeSourceMode.FindAncestor, GetType(DataGridRow), 1)
+            bind.RelativeSource = rs
             colS.Binding = bind
             ItemDataGrid.Columns.Insert(0, colS)
 
 
             Dim col As DataGridTextColumn = ItemDataGrid.FindResource("MCQtyColumn")
             col.Header = mc.Number
-            'Dim format As String = col.Binding.StringFormat
-            Dim qtyBind As New Binding("CustOrderItemDetails")
-            qtyBind.Converter = New QtyConverter
+            Dim qtyBind As New Binding()
+            ' Binds to the Entities attached property, as indexed by colNo
+            qtyBind.Path = New PropertyPath("(0)[(1)].Quantity", MachInfo.EntitiesProperty, colNo)
+            colNo += 1
             qtyBind.StringFormat = "G8"
             qtyBind.TargetNullValue = ""
-            qtyBind.ConverterParameter = mc.OrderDetailID
-            'qtyBind.Mode = BindingMode.OneWay
+            qtyBind.RelativeSource = rs
             col.Binding = qtyBind
-            CType(col.Binding, Binding).ConverterParameter = mc.OrderDetailID
             ItemDataGrid.Columns.Insert(0, col)
         Next
 
@@ -152,6 +188,10 @@ Class MainWindow
                   Where m.Type = MachineType
         End If
 
+        ' Order list
+        Bom = From m In Bom
+              Order By m.Item1
+
         Bom.Load
 
         'Load data by setting the CollectionViewSource.Source property:
@@ -169,7 +209,10 @@ Class MainWindow
     End Sub
 
     Private Sub ItemsGrid_RowEditEnding(sender As Object, e As DataGridRowEditEndingEventArgs) Handles ItemDataGrid.RowEditEnding
-        'ItemDataGrid.CommitEdit(DataGridEditingUnit.Cell, False)
+        If Validation.GetHasError(e.Row) Then
+            e.Cancel = True 'Validation.GetHasError(e.Row)
+            MsgBox(Validation.GetErrors(e.Row)(0).ToString)
+        End If
         If _db.ChangeTracker.HasChanges Then
             Try
                 _db.SaveChanges()
@@ -201,4 +244,52 @@ Class MainWindow
         'e.Handled = True
     End Sub
 
+    'Private Sub DataGrid_Loaded(sender As Object, e As RoutedEventArgs)
+    '    Dim dg As DataGrid = sender
+    '    For Each col In ItemDataGrid.Columns
+    '        Dim newcol As DataGridColumn = col
+    '        ' HACK: make this add child levels automatically
+    '        newcol.Header = newcol.Header.ToString + "child"
+    '        dg.Columns.Add(newcol)
+    '    Next
+    'End Sub
+
+    Private Sub DataGridRowHeader_Click(sender As Object, e As RoutedEventArgs)
+        Dim row As DataGridRow = sender
+        'GetRowLevelByRowHandle
+    End Sub
+
+    Private Sub ItemDataGrid_PreparingCellForEdit(sender As Object, e As DataGridPreparingCellForEditEventArgs) Handles ItemDataGrid.PreparingCellForEdit
+        e.EditingElement.ContextMenu = FindResource("StandardContextMenu")
+        ' TODO: merge context menus as appropriate for filtering the different column data types
+    End Sub
+
+    Private Sub ItemDataGrid_LoadingRow(sender As Object, e As DataGridRowEventArgs) Handles ItemDataGrid.LoadingRow
+
+        ' Add a CustomerOrderItemDetail for each pair of machine columns
+        Dim ml As New List(Of Object)
+        For Each mach In m_machines
+            Dim m = (From cod In CType(e.Row.Item, EJData.Item).CustOrderItemDetails
+                     Where cod.DetailID = mach.DetailID).FirstOrDefault
+            ml.Add(m)
+        Next
+
+        e.Row.SetValue(MachInfo.EntitiesProperty, ml)
+
+    End Sub
+
+    Private Sub StatusCell_PreviewMouseDoubleClick(sender As Object, e As MouseButtonEventArgs)
+        If e.ChangedButton = MouseButton.Left Then
+            If CType(sender, DataGridCell).IsEditing Then
+                EJHelpers.OpenOrderView(CType(CType(sender, DataGridCell).Content, TextBox).Text)
+            Else
+                EJHelpers.OpenOrderView(CType(CType(sender, DataGridCell).Content, TextBlock).Text)
+            End If
+        End If
+        e.Handled = True
+    End Sub
+
+    Private Sub ItemDataGrid_GotFocus(sender As Object, e As RoutedEventArgs)
+        ' TODO: set focus on current cell content for edit on enter?
+    End Sub
 End Class
